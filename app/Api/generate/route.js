dd8cf98045724f62f0eeb1c5113502a024568532
sayanM-25 <sayanmunsi8@gmail.com>
2026-07-12 01:05:27 +0530

    error fix

---
+++ app/Api/generate/route.js
@@ -0,0 +1,17 @@
+import clientPromise from "@/lib/mongodb";
+
+export async function POST(request) {
+  const body = await request.json();
+  const client = await clientPromise;
+
+  const db = client.db("linktree");
+  const collection = db.collection("links");
+
+  const result = await collection.insertOne(body);
+  return Response.json({
+    success: true,
+    error: false,
+    message: "added",
+    result: result,
+  });
+}
